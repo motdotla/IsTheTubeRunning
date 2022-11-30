1d65c6acae82defa7a1ec877c1d29ca8be2883d7
Rob Aleck <mnbf9rca@users.noreply.github.com>
2022-11-30 21:31:51 +0000

    remove 'backend' service

--- backend/controllers/home.js
+++
@@ -1,9 +0,0 @@
-const homeRouter = require('express').Router()
-
-homeRouter.get('/', async (_request, response) => {
-
-  const r = { loaded: 'ok' }
-  response.send(r)
-})
-
-module.exports = homeRouter
