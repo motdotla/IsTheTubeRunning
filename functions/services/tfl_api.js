5af79a05b3a73f3608e34f53872d6b2b74abd11a
Rob Aleck <mnbf9rca@users.noreply.github.com>
2022-12-04 13:06:46 +0000

    cached & non-cached responses have same structure

--- functions/services/tfl_api.js
+++ functions/services/tfl_api.js
@@ -15,31 +15,31 @@ const add_search_params = (url, params) => {
   Object.keys(new_params).forEach((key) => (new_params[key] === undefined ? delete new_params[key] : {}))
   let new_url = url
   for (var p in new_params)
-    if (Object.prototype.hasOwnProperty.call(new_params,p)) {
+    if (Object.prototype.hasOwnProperty.call(new_params, p)) {
       new_url.searchParams.append(p, new_params[p])
     }
   return new_url
 }
 
-async function get_disruption(detailed = false, for_modes = ['tube', 'dlr', 'overground']) {
-  // gets the disruption data from TFL
-  const modes = for_modes.join(',')
-  const cache_key = `disruption-${modes}-${detailed}`
-  const cached_value = query_cache.get(cache_key)
+const structure_cached_value = (cached_value, cache_key) => {
+  // add the cache ttl to the cached value
+  // so that all responses have the same structure
 
-  if (cached_value) {
-    logger.debug(`${cache_key} cache hit`)
-    return cached_value
-  } else {
-    logger.debug(`${cache_key} cache miss`)
-    const disruption_api_query = `Line/Mode/${modes}/Status`
-    const disruption = await query(disruption_api_query, { detail: detailed })
-    query_cache.set(cache_key, disruption.data, disruption.ttl)
-    return disruption
+  const cache_ttl = query_cache.getTtl( cache_key )
+  const currentDate = new Date()
+  let remaining_ttl = 0
+  if (cache_ttl > 0) {
+    remaining_ttl = Math.floor((cache_ttl - currentDate.getTime())/1000)
   }
+  else {
+    remaining_ttl = cache_ttl
+  }
+  return { data: cached_value, ttl: remaining_ttl }
 }
 
 async function query(querystring, params = null) {
+  // makes a query to the TFL API
+  
   const axios = require('axios')
   const tfl_api_root = config.tfl_api_root
   const tfl_app_key = config.TFL_APP_KEY
@@ -47,7 +47,7 @@ async function query(querystring, params = null) {
   let tfl_api_url = new URL(querystring, tfl_api_root)
   // add params to tfl_api_url as search parameters
   if (params) tfl_api_url = add_search_params(tfl_api_url, params)
-  
+
   const tfl_api_headers = {
     'Content-Type': 'application/json',
     'cache-control': 'no-cache',
@@ -69,13 +69,34 @@ async function query(querystring, params = null) {
   return { data, ttl }
 }
 
+async function get_disruption(detailed = false, for_modes = ['tube', 'dlr', 'overground']) {
+  // gets the disruption data from TFL
+  const modes = for_modes.join(',')
+  const cache_key = `disruption-${modes}-${detailed}`
+  const cached_value = query_cache.get(cache_key)
+
+  if (cached_value) {
+    logger.debug(`${cache_key} cache hit`)
+    return structure_cached_value(cached_value, cache_key)
+  } else {
+    logger.debug(`${cache_key} cache miss`)
+    const disruption_api_query = `Line/Mode/${modes}/Status`
+    const disruption = await query(disruption_api_query, { detail: detailed })
+    query_cache.set(cache_key, disruption.data, disruption.ttl)
+    return disruption
+  }
+}
+
+
+
 async function get_line_stoppoints(line_id) {
   // gets the stoppoints for a given line
   const cache_key = `line_stoppoints-${line_id}`
   const cached_value = query_cache.get(cache_key)
   if (cached_value) {
     logger.debug(`${cache_key} cache hit`)
-    return cached_value
+
+    return structure_cached_value(cached_value, cache_key)
   }
   else {
     logger.debug(`${cache_key} cache miss`)
@@ -94,7 +115,7 @@ async function get_all_lines(modes = ['tube', 'dlr', 'overground']) {
   if (cached_value) {
     logger.debug(`${cache_key} cache hit`)
 
-    return cached_value
+    return structure_cached_value(cached_value, cache_key)
   }
   else {
     logger.debug(`${cache_key} cache miss`)
