5bb014ebfac1aecf5e9952d444a0ba9a2a2dc6ce
Rob Aleck <mnbf9rca@users.noreply.github.com>
2022-12-04 11:48:17 +0000

    split disruption_detail

--- functions/controllers/disruption.js
+++ functions/controllers/disruption.js
@@ -2,35 +2,33 @@ const call_tfl = require('../services/tfl_api')
 const logger = require('../utils/logger')
 
 
-async function get_disruption({ res }) {
+async function is_there_disruption() {
 /**
- * Fetches the disruption data from TFL. If there's no disruption, a simple message is returned.
- * if there is disruption, the disruption full set of detailed data is returned, including every impacted station.
- *
- */
+ * Fetches the disruption data from TFL. returns a binary true/false.
+ **/
 
-  // first, check the "light" version of the disruption data
-  // if there's disruption, then call the "detailed" version of the disruption data
-  // the simple version is often 400 lines, the detailed verison is over 10,000 lines
-  // so we want to avoid calling the detailed version if we don't have to
-
-  const simple_disruption = await call_tfl.get_disruption(false)
+  const disruption = await call_tfl.get_disruption(false)
 
   // check if statusSeverity for every lineStatus is 10 (good service)
-  const line_disruption = simple_disruption.map(l => l['lineStatuses'].every((s => s['statusSeverity'] === 10)))
+  const line_disruption = disruption.data.map(l => l['lineStatuses'].every((s => s['statusSeverity'] === 10)))
   // if any of the lines have disruption, then flag this as true
   const is_there_disruption = !line_disruption.every(s => s === true)
+  logger.debug(`is_there_disruption: ${is_there_disruption}`)
 
-  if (is_there_disruption) {
-    // if there's disruption, then call the "detailed" version of the disruption data
-    logger.debug('There is disruption, calling the detailed version of the disruption data')
-    const detailed_disruption = await call_tfl.get_disruption(true)
-    res.status(200).json(detailed_disruption)
-  } else {
-    res.status(200).json(simple_disruption)
-  }
+  return { is_there_disruption, ttl: disruption.ttl }
 
 }
 
+async function disruption_detail() {
+/**
+ * Fetches the disruption data from TFL. returns the detailed disruption data.
+ * can be quite large - several MB - as it includes every impacted station
+ **/
 
-module.exports = { get_disruption }
+  const disruption  = await call_tfl.get_disruption(true)
+
+  return { disruption }
+
+}
+
+module.exports = { is_there_disruption, disruption_detail }
