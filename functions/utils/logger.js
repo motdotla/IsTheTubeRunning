160842402662dee14f1cd5ce115f9fce38f65113
Rob Aleck <mnbf9rca@users.noreply.github.com>
2022-12-05 21:58:26 +0000

    look at using azure logger

--- functions/utils/logger.js
+++ functions/utils/logger.js
@@ -1,10 +1,14 @@
+// const { logger } = require('@azure/event-hubs')
+
+
 const info = (...params) => {
-  console.log(...params)
+  console.info(...params)
 }
 
 const error = (...params) => {
   console.error(...params)
 }
+
 
 const debug = (...params) => {
   console.debug(...params)
