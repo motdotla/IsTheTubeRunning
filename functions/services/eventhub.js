e2a9266c0408aecd524b7c89d474101f0b00ae2d
Rob Aleck <mnbf9rca@users.noreply.github.com>
2022-12-05 21:58:03 +0000

    send final batch

--- functions/services/eventhub.js
+++ functions/services/eventhub.js
@@ -20,24 +20,29 @@ async function publishBatch(batch_array) {
   let batch = await client.createBatch()
   let batch_count = 0
   for (let i = 0; i < batch_array.length; i++) {
-    if (batch_array[i]){
-      let event_item = { body: batch_array[i] }
+    if (batch_array[i]){ // sometimes we get nulls in the array
+      let event_item = { body: batch_array[i], contentType: 'application/json' } // JSON.stringify(batch_array[i]) } //sometimes we need to stringify the object
       // logger.debug('publishBatch event_item: ', event_item)
-      if (!batch.tryAdd(event_item)) {
+      if (!batch.tryAdd(event_item)) { // if the batch is full, send it and start a new one
         batch_count = batch_count + batch.count
         await client.sendBatch(batch)
         batch = await client.createBatch()
-        if (!batch.tryAdd(event_item)) {
+        if (!batch.tryAdd(event_item)) { // if the batch is still full, throw an error
           throw new Error(`Failed to add item to batch: ${event_item}`)
-        } 
-        if (i === batch_array.length - 1) {
+        }
+        /*
+        if (i === batch_array.length - 1) { // if we are at the end of the array, send the last batch
           batch_count = batch_count + batch.count
           await client.sendBatch(batch)
         }
-      } 
+        */
+      }
     }
   }
-  batch_count = batch_count + batch.count
+  if (batch.count > 0){ // send the last batch if there is one
+    logger.debug('sending last batch', batch.count)
+    batch_count = batch_count + batch.count
+    await client.sendBatch(batch) }
   await client.close()
   logger.debug(`published ${batch_count} items to eventhub`)
   return { success: true, submitted: batch_count, total_possible: batch_array.length }
