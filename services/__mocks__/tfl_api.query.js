35ac21a8f29c7f3e597c8707658a90ec0ef66c7d
Rob Aleck <mnbf9rca@users.noreply.github.com>
2022-12-16 12:35:01 +0000

    move all TFL API mocks to __mocks__

--- services/__mocks__/tfl_api.query.js
+++ services/__mocks__/tfl_api.query.js
@@ -1,20 +1,122 @@
-const responses = require('../tests/tfl_api.query_responses')
+const responses = require('./tfl_api.query_responses')
 console.log('mocking TfL API')
 
 
+const check_params = (params, expected_params) => {
+  /**
+   * Checks that the params object contains the expected params
+   * @param {Object} params - params object
+   * @param {Object} expected_params - expected params object
+   * @returns {Boolean} - true if params contains expected_params
+   *
+   */
+  if (params === undefined) {
+    return false
+  }
+  for (const [key, value] of Object.entries(expected_params)) {
+    if (params[key] !== value) {
+      return false
+    }
+  }
+  return true
+}
+
+
 let originalModule = jest.requireActual('../tfl_api.query')
-const mockquery = (url, priority) => {
+/*
+const mockquery = (url, params) => {
   /**
    * Mocks the query function from tfl_api.query
-   */
-  console.log('mocking query', url, priority)
-  return { data: url }
+   *
+  console.log('mocking query', url, params)
+
+  switch (url.toLowerCase()) {
+    case 'line/victoria/route/sequence/all':
+      return responses.get_line_stoppoints_in_order_victoria_no_crowding
+    case 'line/victoria/stoppoints':
+      return responses.get_line_stoppoints_victoria
+    case 'line/mode/tube/status':
+      if (check_params(params, { detailed: 'true' })) {
+        return responses.get_disruption_tube_detailed
+      } else {
+        return responses.get_disruption_tube
+      }
+    case 'line/mode/tube,overground/status':
+      if (check_params(params, { detailed: 'true' })) {
+        return responses.get_disruption_tube_overground_detailed
+      } else {
+        return responses.get_disruption_tube_overground
+      }
+    case 'line/mode/tube/route':
+      return responses.get_lines_for_mode_tube
+    case 'line/mode/tube,overground/route':
+      return responses.get_lines_for_mode_tube_overground
+    default:
+      throw new Error(`No mock response for ${url}`)
+
+  }
 }
 
 const mocked_module = {
   ...originalModule,
   query: mockquery
 }
+
+*/
+const axios = require('axios')
+jest.mock('axios')
+const mockquery = (url, params) => {
+  /**
+   * Mocks the query function from tfl_api.query
+   */
+  console.log('mocking query', url, params)
+
+  switch (url.toLowerCase()) {
+  case 'line/victoria/route/sequence/all':
+    axios.get.mockResolvedValue(responses.get_line_stoppoints_in_order_victoria_no_crowding)
+    return originalModule.query(url, params)
+
+  case 'line/victoria/stoppoints':
+    axios.get.mockResolvedValue(responses.get_line_stoppoints_victoria)
+    return originalModule.query(url, params)
+
+  case 'line/mode/tube/status':
+    if (check_params(params, { detailed: 'true' })) {
+      axios.get.mockResolvedValue(responses.get_disruption_tube_detailed)
+    } else {
+      axios.get.mockResolvedValue(responses.get_disruption_tube)
+    }
+    return originalModule.query(url, params)
+
+  case 'line/mode/tube,overground/status':
+    if (check_params(params, { detailed: 'true' })) {
+      axios.get.mockResolvedValue(responses.get_disruption_tube_overground_detailed)
+    } else {
+      axios.get.mockResolvedValue(responses.get_disruption_tube_overground)
+    }
+    return originalModule.query(url, params)
+
+  case 'line/mode/tube/route':
+    axios.get.mockResolvedValue(responses.get_lines_for_mode_tube)
+    return originalModule.query(url, params)
+
+  case 'line/mode/tube,overground/route':
+    axios.get.mockResolvedValue(responses.get_lines_for_mode_tube_overground)
+    return originalModule.query(url, params)
+
+  default:
+    throw new Error(`No mock response for ${url}`)
+
+  }
+}
+
+const mocked_module = {
+  ...originalModule,
+  query: mockquery
+}
+
+
+
 
 //originalModule['query'] = mockquery
 console.log('mocked')
--- services/tests/tfl_api.test.js
+++ services/tests/tfl_api.test.js
@@ -4,10 +4,10 @@ const { describe, expect, test } = require('@jest/globals')
 
 const tfl_api = require('../tfl_api')
 
-const axios = require('axios')
-jest.mock('axios')
 
-const tfl_api_responses = require('./tfl_api.query_responses')
+const query = require('../tfl_api.query')
+jest.mock('../tfl_api.query')
+
 const tfl_sdk_responses = require('./tfl_api.sdk_responses')
 
 const fs = require('fs')
@@ -65,13 +65,11 @@ describe('TfL tests', () => {
   describe('TfL calls to get line stoppoints', () => {
     describe('calls without caching', () => {
       test('calls TFL API to get ordered Victoria line stoppoints', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_line_stoppoints_in_order_victoria_no_crowding)
         const actual_response = await tfl_api.get_line_stoppoints_in_order('victoria')
         const expected_response = tfl_sdk_responses.get_line_stoppoints_in_order_victoria_no_crowding
         expect(actual_response).toMatchObject(expected_response)
       })
       test('calls TFL API to get default Victoria line trains', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_line_stoppoints_victoria)
         const actual_response = await tfl_api.get_line_stoppoints('victoria')
         const expected_response = tfl_sdk_responses.get_line_stoppoints_victoria
         expect(actual_response).toMatchObject(expected_response)
@@ -79,14 +77,12 @@ describe('TfL tests', () => {
     })
     describe('calls with caching', () => {
       test('calls TFL API to get ordered Victoria line stoppoints', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_line_stoppoints_in_order_victoria_no_crowding)
         const expected_response = tfl_sdk_responses.get_line_stoppoints_in_order_victoria_no_crowding
         const first_response = await tfl_api.get_line_stoppoints_in_order('victoria')
         const actual_response = await tfl_api.get_line_stoppoints_in_order('victoria')
         test_first_and_actual_response(first_response, actual_response, expected_response)
       })
       test('calls TFL API to get default Victoria line trains', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_line_stoppoints_victoria)
         const expected_response = tfl_sdk_responses.get_line_stoppoints_victoria
         const first_response = await tfl_api.get_line_stoppoints('victoria')
         const actual_response = await tfl_api.get_line_stoppoints('victoria')
@@ -98,31 +94,26 @@ describe('TfL tests', () => {
   describe('TfL calls to get disruption', () => {
     describe('calls without caching', () => {
       test('calls TFL API to get disruption on tube, no detail', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_disruption_tube)
         const actual_response = await tfl_api.get_disruption(['tube'])
         const expected_response = tfl_sdk_responses.get_disruption_tube
         expect(actual_response).toMatchObject(expected_response)
       })
       test('calls TFL API to get disruption on tube,overground, no detail', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_disruption_tube_overground)
         const actual_response = await tfl_api.get_disruption(['tube', 'overground'])
         const expected_response = tfl_sdk_responses.get_disruption_tube_overground
         expect(actual_response).toMatchObject(expected_response)
       })
       test('calls TFL API to get disruption on tube, with detail', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_disruption_tube_detailed)
         const actual_response = await tfl_api.get_disruption(['tube'], true)
         const expected_response = tfl_sdk_responses.get_disruption_tube_detailed
         expect(actual_response).toMatchObject(expected_response)
       })
       test('calls TFL API to get disruption on tube,overground, with detail', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_disruption_tube_overground_detailed)
         const actual_response = await tfl_api.get_disruption(['tube', 'overground'], true)
         const expected_response = tfl_sdk_responses.get_disruption_tube_overground_detailed
         expect(actual_response).toMatchObject(expected_response)
       })
       test('calls TFL API to get disruption with string for_modes, no detail', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_disruption_tube)
         await expect(tfl_api.get_disruption('tube'))
           .rejects
           .toThrowError('must be an array')
@@ -130,28 +121,24 @@ describe('TfL tests', () => {
     })
     describe('calls with caching', () => {
       test('calls TFL API to get disruption on tube, no detail', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_disruption_tube)
         const first_response = await tfl_api.get_disruption(['tube'])
         const actual_response = await tfl_api.get_disruption(['tube'])
         const expected_response = tfl_sdk_responses.get_disruption_tube
         test_first_and_actual_response(first_response, actual_response, expected_response)
       })
       test('calls TFL API to get disruption on tube,overground, no detail', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_disruption_tube_overground)
         const first_response = await tfl_api.get_disruption(['tube', 'overground'])
         const actual_response = await tfl_api.get_disruption(['tube', 'overground'])
         const expected_response = tfl_sdk_responses.get_disruption_tube_overground
         test_first_and_actual_response(first_response, actual_response, expected_response)
       })
       test('calls TFL API to get disruption on tube, with detail', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_disruption_tube_detailed)
         const first_response = await tfl_api.get_disruption(['tube'], true)
         const actual_response = await tfl_api.get_disruption(['tube'], true)
         const expected_response = tfl_sdk_responses.get_disruption_tube_detailed
         test_first_and_actual_response(first_response, actual_response, expected_response)
       })
       test('calls TFL API to get disruption on tube,overground, with detail', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_disruption_tube_overground_detailed)
         const first_response = await tfl_api.get_disruption(['tube', 'overground'], true)
         const actual_response = await tfl_api.get_disruption(['tube', 'overground'], true)
         const expected_response = tfl_sdk_responses.get_disruption_tube_overground_detailed
@@ -163,13 +150,11 @@ describe('TfL tests', () => {
   describe('TfL calls to get lines for a mode', () => {
     describe('calls without caching', () => {
       test('calls TFL API to get lines for "tube"', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_lines_for_mode_tube)
         const actual_response = await tfl_api.get_lines_for_mode(['tube'])
         const expected_response = tfl_sdk_responses.get_lines_for_mode_tube
         expect(actual_response).toMatchObject(expected_response)
       })
       test('calls TFL API to get lines for "tube, overground"', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_lines_for_mode_tube_overground)
         const actual_response = await tfl_api.get_lines_for_mode(['tube', 'overground'])
         const expected_response = tfl_sdk_responses.get_lines_for_mode_tube_overground
         expect(actual_response).toMatchObject(expected_response)
@@ -177,14 +162,12 @@ describe('TfL tests', () => {
     })
     describe('calls with caching', () => {
       test('calls TFL API to get lines for "tube"', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_lines_for_mode_tube)
         const first_response = await tfl_api.get_lines_for_mode(['tube'])
         const actual_response = await tfl_api.get_lines_for_mode(['tube'])
         const expected_response = tfl_sdk_responses.get_lines_for_mode_tube
         test_first_and_actual_response(first_response, actual_response, expected_response)
       })
       test('calls TFL API to get lines for "tube, overground"', async () => {
-        axios.get.mockResolvedValue(tfl_api_responses.get_lines_for_mode_tube_overground)
         const first_response = await tfl_api.get_lines_for_mode(['tube', 'overground'])
         const actual_response = await tfl_api.get_lines_for_mode(['tube', 'overground'])
         const expected_response = tfl_sdk_responses.get_lines_for_mode_tube_overground
